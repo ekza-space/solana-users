c4e5469d6f6a9b233061e8b7a0cafed2da7ffe90
Wotori <10486621+wotori@users.noreply.github.com>
2024-10-07 12:41:45 +0300

    basic partitions

--- programs/users/src/lib.rs
+++ programs/users/src/lib.rs
@@ -3,17 +3,34 @@ use anchor_lang::prelude::*;
 declare_id!("CrxQKgLZU7bnm5yPsn4tMvnK9qBWDCtzoz3NCvk39eV3");
 
 // Constants for PDA seeds
-pub const USERS_LIST_SEED: &[u8] = b"ekza_users_list";
-pub const USER_PROFILE_SEED: &[u8] = b"ekza_user_profile";
+pub const PARTITIONS_LIST_SEED: &[u8] = b"partitions_list";
+pub const PARTITION_SEED: &[u8] = b"partition";
+pub const USER_PROFILE_SEED: &[u8] = b"user_profile";
 
 #[program]
 pub mod user_profiles {
     use super::*;
 
-    // Initialization of the central account for storing the list of users
+    // Initialization of the root account for storing the list of partitions
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        let users_list = &mut ctx.accounts.users_list;
-        users_list.users = Vec::new();
+        let partitions_list = &mut ctx.accounts.partitions_list;
+        partitions_list.partitions = Vec::new();
+        Ok(())
+    }
+
+    // Creating a new partition (to be called when existing partitions are full)
+    pub fn create_partition(ctx: Context<CreatePartition>) -> Result<()> {
+        let partitions_list = &mut ctx.accounts.partitions_list;
+        let partition = &mut ctx.accounts.partition;
+
+        // Assign partition id
+        let partition_id = partitions_list.partitions.len() as u64;
+        partition.id = partition_id;
+        partition.users = Vec::new();
+
+        // Add the partition's key to the partitions list
+        partitions_list.partitions.push(partition.key());
+
         Ok(())
     }
 
@@ -25,17 +42,36 @@ pub mod user_profiles {
         twitter_link: String,
         website_link: String,
     ) -> Result<()> {
-        let profile = &mut ctx.accounts.user_profile;
-        profile.owner = ctx.accounts.user.key();
-        profile.nickname = nickname;
-        profile.description = description;
-        profile.twitter_link = twitter_link;
-        profile.website_link = website_link;
-        profile.following = Vec::new();
+        let partitions_list = &mut ctx.accounts.partitions_list;
+        let partition = &mut ctx.accounts.partition;
+        let user_profile = &mut ctx.accounts.user_profile;
 
-        // Adding the user to the general list
-        let users_list = &mut ctx.accounts.users_list;
-        users_list.users.push(ctx.accounts.user.key());
+        // Check if the partition is filled
+        if partition.users.len() >= Partition::MAX_USERS {
+            // Check if the partition limit is exceeded
+            if partitions_list.partitions.len() >= PartitionsList::MAX_PARTITIONS {
+                return Err(MyError::MaxPartitionsReached.into());
+            }
+
+            // Create a new partition
+            let _new_partition = Partition::default(); // Initialize a new partition account here
+            partition.id = partitions_list.partitions.len() as u64;
+            partition.users = Vec::new();
+            partitions_list.partitions.push(partition.key());
+        }
+
+        // Initialize the user profile
+        user_profile.owner = ctx.accounts.user.key();
+        user_profile.nickname = nickname;
+        user_profile.description = description;
+        user_profile.twitter_link = twitter_link;
+        user_profile.website_link = website_link;
+        user_profile.following = Vec::new();
+        user_profile.partition_id = partition.id;
+        user_profile.registration_timestamp = Clock::get()?.unix_timestamp;
+
+        // Add the user to the partition
+        partition.users.push(ctx.accounts.user.key());
 
         Ok(())
     }
@@ -49,7 +85,7 @@ pub mod user_profiles {
             MyError::Unauthorized
         );
 
-        // Adding the address to the following list if it is not already there
+        // Add the address to the following list if it is not already there
         if !profile.following.contains(&target_user) {
             profile.following.push(target_user);
         }
@@ -59,7 +95,7 @@ pub mod user_profiles {
 
     pub fn update_profile(
         ctx: Context<UpdateProfile>,
-        nickname: Option<String>, // Use Option, don't update is None
+        nickname: Option<String>,
         description: Option<String>,
         twitter_link: Option<String>,
         website_link: Option<String>,
@@ -90,24 +126,26 @@ pub mod user_profiles {
 
     // Deleting a user profile
     pub fn delete_profile(ctx: Context<DeleteProfile>) -> Result<()> {
-        let profile = &mut ctx.accounts.user_profile;
+        let user_profile = &ctx.accounts.user_profile;
+        let partition = &mut ctx.accounts.partition;
+
         require_keys_eq!(
-            profile.owner,
+            user_profile.owner,
             ctx.accounts.user.key(),
             MyError::Unauthorized
         );
 
-        // Removing the user from the general list
-        let users_list = &mut ctx.accounts.users_list;
-        if let Some(pos) = users_list
+        // Remove the user from the partition
+        if let Some(pos) = partition
             .users
             .iter()
             .position(|&x| x == ctx.accounts.user.key())
         {
-            users_list.users.remove(pos);
+            partition.users.remove(pos);
+        } else {
+            return Err(MyError::UserNotFoundInPartition.into());
         }
 
-        // Closing the profile account (funds are returned to the user)
         Ok(())
     }
 }
@@ -117,18 +155,51 @@ pub struct Initialize<'info> {
     #[account(
         init,
         payer = user,
-        space = 8 + UsersList::MAX_SIZE,
-        seeds = [USERS_LIST_SEED],
+        space = 8 + PartitionsList::MAX_SIZE,
+        seeds = [PARTITIONS_LIST_SEED],
         bump,
     )]
-    pub users_list: Account<'info, UsersList>,
+    pub partitions_list: Account<'info, PartitionsList>,
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
+pub struct CreatePartition<'info> {
+    #[account(
+        mut,
+        seeds = [PARTITIONS_LIST_SEED],
+        bump,
+    )]
+    pub partitions_list: Account<'info, PartitionsList>,
+    #[account(
+        init,
+        payer = payer,
+        space = 8 + Partition::MAX_SIZE,
+        seeds = [PARTITION_SEED, (partitions_list.partitions.len() as u64).to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub partition: Account<'info, Partition>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
 pub struct CreateProfile<'info> {
+    #[account(
+        mut,
+        seeds = [PARTITIONS_LIST_SEED],
+        bump,
+    )]
+    pub partitions_list: Account<'info, PartitionsList>,
+    #[account(
+        mut,
+        seeds = [PARTITION_SEED, partition.id.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub partition: Account<'info, Partition>,
     #[account(
         init,
         payer = user,
@@ -137,8 +208,6 @@ pub struct CreateProfile<'info> {
         bump,
     )]
     pub user_profile: Account<'info, UserProfile>,
-    #[account(mut, seeds = [USERS_LIST_SEED], bump)]
-    pub users_list: Account<'info, UsersList>,
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
@@ -150,10 +219,10 @@ pub struct UpdateProfile<'info> {
         mut,
         seeds = [USER_PROFILE_SEED, user.key().as_ref()],
         bump,
-        has_one = owner // This ensures that the profile owner matches the 'owner'
+        has_one = owner
     )]
     pub user_profile: Account<'info, UserProfile>,
-    pub owner: AccountInfo<'info>, // Owner verification field
+    pub owner: Signer<'info>,
     #[account(mut)]
     pub user: Signer<'info>,
 }
@@ -164,10 +233,11 @@ pub struct FollowUser<'info> {
         mut,
         seeds = [USER_PROFILE_SEED, user.key().as_ref()],
         bump,
-        has_one = owner // This ensures the user_profile's owner is the same as 'owner' account
+        has_one = owner
     )]
     pub user_profile: Account<'info, UserProfile>,
-    pub owner: AccountInfo<'info>, // Add this field to satisfy the 'has_one' requirement
+    pub owner: Signer<'info>,
+    #[account(mut)]
     pub user: Signer<'info>,
 }
 
@@ -178,12 +248,14 @@ pub struct DeleteProfile<'info> {
         close = user,
         seeds = [USER_PROFILE_SEED, user.key().as_ref()],
         bump,
-        has_one = owner // This ensures the user_profile's owner is the same as 'owner' account
     )]
     pub user_profile: Account<'info, UserProfile>,
-    pub owner: AccountInfo<'info>, // Add this field to satisfy the 'has_one' requirement
-    #[account(mut, seeds = [USERS_LIST_SEED], bump)]
-    pub users_list: Account<'info, UsersList>,
+    #[account(
+        mut,
+        seeds = [PARTITION_SEED, user_profile.partition_id.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub partition: Account<'info, Partition>,
     #[account(mut)]
     pub user: Signer<'info>,
 }
@@ -195,7 +267,9 @@ pub struct UserProfile {
     pub description: String,
     pub twitter_link: String,
     pub website_link: String,
-    pub following: Vec<Pubkey>, // List of addresses that the user is following
+    pub following: Vec<Pubkey>,
+    pub partition_id: u64,
+    pub registration_timestamp: i64,
 }
 
 impl UserProfile {
@@ -210,21 +284,48 @@ impl UserProfile {
         + 4 + Self::MAX_DESCRIPTION_LEN // description
         + 4 + Self::MAX_TWITTER_LEN // twitter_link
         + 4 + Self::MAX_WEBSITE_LEN // website_link
+        + 8 // partition_id
+        + 8 // registration_timestamp
         + 4 + (32 * Self::MAX_FOLLOWING_LEN); // following
 }
 
 #[account]
-pub struct UsersList {
+pub struct PartitionsList {
+    pub partitions: Vec<Pubkey>,
+}
+
+impl PartitionsList {
+    const MAX_PARTITIONS: usize = 250; // Maximum number of partitions
+    const MAX_SIZE: usize = 4 + (32 * Self::MAX_PARTITIONS); // 4 bytes for length prefix
+}
+
+#[account]
+pub struct Partition {
+    pub id: u64,
     pub users: Vec<Pubkey>,
 }
 
-impl UsersList {
-    const MAX_USERS: usize = 250; // Maximum number of users
-    const MAX_SIZE: usize = 4 + (32 * Self::MAX_USERS); // 4 bytes for length prefix
+impl Partition {
+    const MAX_USERS: usize = 250; // Maximum number of users per partition
+    const MAX_SIZE: usize = 8 // id
+        + 4 + (32 * Self::MAX_USERS); // users
+}
+
+impl Default for Partition {
+    fn default() -> Self {
+        Self {
+            id: 0, // or other value
+            users: Vec::new(),
+        }
+    }
 }
 
 #[error_code]
 pub enum MyError {
     #[msg("Access denied")]
     Unauthorized,
+    #[msg("User not found in partition")]
+    UserNotFoundInPartition,
+    #[msg("Max partitions reached")]
+    MaxPartitionsReached,
 }
